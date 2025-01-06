7b5af8089551cde323654097b3f47434eee47574
whyk0426 <kkyyss426@gmail.com>
2025-01-06 14:04:43 +0900

    rotating added

--- merge_map/merge_map.py
+++ merge_map/merge_map.py
@@ -2,12 +2,45 @@ import rclpy
 from rclpy.node import Node
 from nav_msgs.msg import OccupancyGrid
 import numpy as np
+import math
 
-n = 3
+PI = math.pi
+
+n = 3   
+theta = [0, 0, PI/4]  
 xd = [0, 0, 0.5]  
 yd = [0, 0.5, 0.5]  
 
 maps_list = [f"map{i}" for i in range(n)] 
+
+def rotate_map(map_data, angle):
+    robot_x = map_data.info.origin.position.x
+    robot_y = map_data.info.origin.position.y
+    rotated_map_data = np.zeros((map_data.info.height, map_data.info.width), dtype=np.int8)
+
+    cos_th = np.cos(angle)
+    sin_th = np.sin(angle)
+
+    for i in range(map_data.info.height):
+        for j in range(map_data.info.width):
+            x = j * map_data.info.resolution
+            y = i * map_data.info.resolution
+
+            rotated_x = cos_th * (x + robot_x) - sin_th * (y + robot_y) - robot_x
+            rotated_y = sin_th * (x + robot_x) + cos_th * (y + robot_y) - robot_y
+
+            new_j = int((rotated_x) / map_data.info.resolution)
+            new_i = int((rotated_y) / map_data.info.resolution)
+
+            if 0 <= new_i < map_data.info.height and 0 <= new_j < map_data.info.width:
+                rotated_map_data[new_i, new_j] = map_data.data[i * map_data.info.width + j]
+            
+    rotated_map_msg = OccupancyGrid()
+    rotated_map_msg.header = map_data.header
+    rotated_map_msg.info = map_data.info
+    rotated_map_msg.data = rotated_map_data.flatten().tolist()
+
+    return rotated_map_msg
 
 def merge_maps(*maps):
     merged_map = OccupancyGrid()
@@ -29,7 +62,7 @@ def merge_maps(*maps):
     merged_map.data = [-1] * (merged_map.info.width * merged_map.info.height)
     ratio = [0] * (merged_map.info.width * merged_map.info.height)
     
-    for k in range(len(maps)): 
+    for k in range(len(maps)):
         for y in range(maps[k].info.height):
             for x in range(maps[k].info.width):
                 i = x + y * maps[k].info.width
@@ -60,10 +93,11 @@ class MergeMapNode(Node):
             setattr(self, maps_list[i], None)
 
     def map_callback(self, msg, map_id):
-            setattr(self, maps_list[map_id], msg)
-            if all(getattr(self, maps_list[j]) is not None for j in range(n) if j != map_id):
-                merged_msg = merge_maps(*(getattr(self, maps_list[k]) for k in range(n)))
-                self.publisher.publish(merged_msg)
+        setattr(self, maps_list[map_id], msg)
+        if all(getattr(self, maps_list[j]) is not None for j in range(n) if j != map_id):
+            rotated_maps = [rotate_map(getattr(self, maps_list[k]), theta[k]) for k in range(n)]  
+            merged_msg = merge_maps(*rotated_maps) 
+            self.publisher.publish(merged_msg)  
 
 def main(args=None):
     rclpy.init(args=args)
